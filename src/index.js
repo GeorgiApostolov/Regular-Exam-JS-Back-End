540d8df63a42f38eda7059221d322872e7e0a1d8
GeorgiApostolov <74099975+GeorgiApostolov@users.noreply.github.com>
2025-10-26 10:57:23 +0200

    init project and express

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import routes from "./routes.js";
+
+const app = express();
+
+app.use(express.static("src/public"));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(routes);
+
+app.listen(3000, () =>
+  console.log("Server is listening on http://localhost:3000...")
+);
